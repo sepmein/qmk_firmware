f5a90691449759075d178f19aa52f9e0ce923d6e
sepmein <sepmein@gmail.com>
2021-02-27 20:30:29 +0100

    feat: modified redox/sepmein

--- keyboards/redox/keymaps/sepmein/keymap.c
+++ keyboards/redox/keymaps/sepmein/keymap.c
@@ -48,21 +48,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSPO ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_LCBR ,KC_LT   ,        KC_GT   ,KC_RCBR ,KC_N    ,KC_M    ,KC_COLN ,KC_DQUO ,KC_SLSH ,KC_RSPC ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     MO(2)   ,MO(3)   ,KC_LEAD ,KC_LALT ,     KC_CCM      ,KC_SPC  ,MO(1)   ,        KC_ENT  ,KC_BSPC ,    KC_GDOT      ,KC_RALT ,KC_UNDS ,MO(3)   ,MO(2)
+     MO(2)   ,MO(3)   ,KC_LEAD ,KC_LALT ,     KC_CCM      ,KC_SPC  ,MO(1)   ,        KC_ENT  ,KC_BSPC ,    KC_GDOT      ,KC_P1   ,KC_P2   ,KC_P3   ,KC_P4
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
   [_SYMB] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F1   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                                            XXXXXXX ,KC_P7   ,KC_P8   ,KC_P9   ,KC_PMNS ,KC_F6   ,
+     KC_F1   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                                            XXXXXXX ,KC_P7   ,KC_P8   ,KC_P9   ,XXXXXXX ,KC_F6   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_F2   ,XXXXXXX ,XXXXXXX ,KC_UP   ,XXXXXXX ,XXXXXXX ,_______ ,                          _______ ,KC_PSLS ,KC_P4   ,KC_P5   ,KC_P6   ,KC_PPLS ,KC_F7   ,
+     KC_F2   ,XXXXXXX ,XXXXXXX ,KC_UP   ,XXXXXXX ,XXXXXXX ,_______ ,                          _______ ,KC_PSLS ,KC_P7   ,KC_P8   ,KC_P9   ,KC_PMNS ,KC_F7   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_F3   ,XXXXXXX ,KC_LEFT ,KC_DOWN ,KC_RIGHT,XXXXXXX ,_______ ,                          _______ ,KC_PAST ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,KC_F8   ,
+     KC_F3   ,XXXXXXX ,KC_LEFT ,KC_DOWN ,KC_RIGHT,XXXXXXX ,_______ ,                          _______ ,KC_PAST ,KC_P4   ,KC_P5   ,KC_P6   ,KC_PPLS ,KC_F8   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_F4   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,_______ ,        _______ ,_______ ,XXXXXXX ,KC_P0   ,KC_P0   ,KC_PDOT ,XXXXXXX ,KC_F9   ,
+     KC_F4   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,_______ ,        _______ ,_______ ,XXXXXXX ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,KC_F9   ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_F5   ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_F10 
+     KC_F5   ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX ,     KC_P0   ,KC_PDOT ,XXXXXXX ,KC_F10 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -102,19 +102,21 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(KC_F) {
-      // Anything you can do in a macro.
-      SEND_STRING("QMK is awesome.");
+    SEQ_ONE_KEY(KC_C) {
+      register_code(KC_LSHIFT);
+      register_code(KC_LGUI);
+      tap_code(KC_BTN1);
+      tap_code(KC_BTN1);
+      unregister_code(KC_LSHIFT);
+      unregister_code(KC_LGUI);
     }
 
-    SEQ_TWO_KEYS(KC_P, KC_S) {
+    SEQ_ONE_KEY(KC_S) {
       SEND_STRING("-Scarlet87");
     }
-
-    SEQ_TWO_KEYS(KC_P, KC_M) {
+    SEQ_ONE_KEY(KC_M) {
       SEND_STRING("-Maroon87");
     }
-    
   }
 }
 
